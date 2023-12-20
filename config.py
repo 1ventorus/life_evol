7f7e263b85cf8fceab642a7db6d0e81242f453ea
1ventorus <113920033+1ventorus@users.noreply.github.com>
2023-12-20 15:30:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python
+
+# import
+from asyncio import*
 import os
 import time
+import random
 
+
+# affichage
 BANNER = """
   ▄█        ▄█     ▄████████    ▄████████         ▄████████  ▄█    █▄   ▄██████▄   ▄█       
  ███       ███    ███    ███   ███    ███        ███    ███ ███    ███ ███    ███ ███       
@@ -21,14 +28,17 @@ Voici la liste des commandes :
     - augmenter : augmente le temps passé par tour de simulation (max 10)
     - reduire : déduit le temps passé par tour de simulation (min 1)
     - reset : réinitialise le monde pour recommencer
+    - GMode years : modifier l'année de simulation
+    - GMode mutate : modifie les mutation aleatoirement
 
 vitesse de simulation : 1 correspond a 1 siècle
 """
 
-# Valeurs d'évolution possibles
+
 uni_cell = "organisme unicellulaire"
 plurial_cell = "organisme pluricellulaire"
-cell = "cellule"
+# valeurs d'évolution possibles
+
 eye = "système organique des yeux"
 human_foot = "pied humain"
 human_hand = "main humaine"
@@ -36,16 +46,16 @@ monkey_foot = "pied de singe"
 griffe = "griffe"
 tail = "queue"
 wing = "ailes"
+horn= "corne"
+tree_eyes= "trois yeux"
 
-# Valeurs d'évolution des organismes
+nbr_sim=0
+
+# valeurs d'évolution des organismes
 quantity_animals_type = 1
 speed = 5
 
-# État des mutations
-yes = "oui"
-no = "non"
-in_take = "en train de se développer"
-
+# gestion de la simulation
 class WorldSim:
     def __init__(self):
         self.time_step = 1
@@ -53,73 +63,152 @@ class WorldSim:
         self.repetition_count = speed
         self.is_running = False
         self.population = 2
+        self.years= 0
+        self.cell=uni_cell
 
+    # lancement de simulation 
     def advance_time(self):
         for _ in range(self.repetition_count):
             self.advance_word()
             print("Simulation en cours...")
-            self.population *= 2
-            self.time_step += 1
+            self.cell = plurial_cell
+
+            self.population *=2
+
+            self.time_step += 1        
 
         if self.time_step > self.simulation_duration:
+            if self.population >= 10000000000000:
+                self.population -= 1000000000000   
+
+            elif self.population >= 10000000:
+                self.population -= 20000
+
+            elif self.population >= 1000:
+                self.population -= 100
+
+            elif self.population <= 100:
+                pass
+            self.years =  self.years+self.repetition_count*100
             self.stop_simulation()
 
+    # fait avancer la simulation dce 1 tour avec un temps de pause
     def advance_word(self):
-        # Simulate the word.advance_time() function
         time.sleep(0.1)
 
+    # stop la simulation 
     def stop_simulation(self):
         self.is_running = False
 
+    # reset la simulation entière
     def reset_simulation(self):
         self.population = 2
+        self.years = 0
         self.is_running = True
 
+    # indique au système que la simulation est en marche
     def is_simulation_running(self):
         return self.is_running
 
-def save():
+WS = WorldSim()
+
+# mutation
+def mutating():
+    values = [eye, human_foot, human_hand, monkey_foot, griffe, tail, wing, horn, tree_eyes]
+    associations = {}
+
+    for value in values:
+        association = random.choice(["oui", "non", "en developpement"])
+        associations[value] = association
+
+    print("Mutation actuelle:")
+    print("{:<30} {}".format("Mutation", "État"))
+    print("-" * 40)
+    for key, value in associations.items():
+        print("{:<30} {}".format(key, value))
+muting = mutating()
+
+# système de sauvegarde
+def save(associations, WS):
     with open("save.txt", "w+") as fichier:
-        fichier.write(f"mutated={WS.mutated}\n")
+        fichier.write(f"mutated={muting}\n")
         fichier.write(f"population={WS.population}\n")
+        fichier.write(f"cell={WS.cell}\n")
+        fichier.write(f"years={WS.years}\n")
 
 def load():
     with open("save.txt", "r") as fichier:
         for line in fichier:
             key, value = line.strip().split("=")
-            if key == "population":
+            if key == "mutated":
+                pass
+            
+            elif key == "population":
                 WS.population = int(value)
 
+            elif key == "cell":
+                WS.cell = value
+
+            elif key == "years":
+                WS.years = int(value)
+            
+
+# simulation
 def sim():
-    WS = WorldSim()
+    associations = {}  
+    mutate = 6
     while True:
         os.system("cls")
         print(BANNER)
         print(HELP)
-        print("quantité actuel d'etre vivant ", WS.population)
-        print("vitesse de simulation : ", WS.repetition_count)
-        print("Exécutez des commandes pour modifier l'avancement du monde.")
+        print("quantité actuel d'être vivant ", WS.population, "k")
+        print("vitesse de simulation : ", WS.repetition_count, "siecle par simulation")
+        print("être vivant", WS.cell)
+        print("vous en êtes à l'année", WS.years)
+        mutating()
+        print("\nExécutez des commandes pour modifier l'avancement du monde.")
+        
         god = input(">>> ")
 
+        # invoque la fonction WS.advance_time()
         if god == "lancer":
             WS.advance_time()
 
+        # augmente le temps écoulé entre chaque simulation
         elif god == "augmenter":
             WS.repetition_count += 1
+        # réduis le temps écoulé entre chaque simulation
         elif god == "reduire":
             WS.repetition_count -= 1
 
+        # sauvegarde l'état de la simulation
         elif god == "save":
-            save()
+            save(associations, WS)
 
+        # charge le l'état de la simulation sauvegardé
         elif god == "load":
             load()
 
+        # modifie manuellement l'année a la quelle se trouve la simulation
+        elif god == "GMode years":
+            print("en quelle année souhaitez-vous être (valeur numérique entière uniquement)?")
+            counter = int(input(">>>"))
+            WS.years = counter
+
+        # permet de modifier les mution aléatoirement
+        elif god == "GMode mutate":
+            pass
+
+        # permet de reset l'état de la simulation
         elif god == "reset":
             WS.reset_simulation()
 
+        # permet de fermer le simulateur
         elif god == "close":
             print("au revoir !")
             time.sleep(2)
             os.system("cls")
             break
+
+
+
